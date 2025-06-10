853c0874ad3965cab4f9711f27750d3401eb9ebd
sethbehar <sethbehar@gmail.com>
2025-06-09 20:40:28 -0400

    remove proxys - unnecessary

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  server: {
-    proxy: {
-      // any request starting with /api/... will be forwarded
-      '/create-checkout-session': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
-  }
 })
 
 
