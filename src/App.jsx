262ad3ddd432f668069a2ce57275fde0a0c56c30
sethbehar <sethbehar@gmail.com>
2025-06-09 20:40:47 -0400

    save user to db on login

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,43 @@ import Payment from './components/Payment'
 import DashboardLayout from './dashboard/DashboardLayout';
 import { Routes, Route } from 'react-router-dom'
 import Signup from './components/Signup';
+import { useAuth, useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const { user } = useUser()
+  const { getToken } = useAuth()
+
+
+  useEffect(() => {
+    if (!user) return;
+
+    (async () => {
+      try {
+        // getToken (no template for now, to simplify)
+        const token = await getToken();
+
+        // fire the upsert
+        const res = await fetch('http://localhost:8000/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log('status ', res.status);
+      } catch (err) {
+        console.error('error in upsert effect', err);
+      }
+    })(); 
+  }, [user, getToken]);
 
   return (
     <Routes>
       <Route path="/" element={
-          <div id="webcrumbs">
-          <div className="bg-gradient-to-br from-green-200 to-green-800 min-h-screen">
+        <div id="webcrumbs">
+          <div className="bg-white min-h-screen">
             <div className="container mx-auto px-4 py-8">
               <Header />
               <Hero />
